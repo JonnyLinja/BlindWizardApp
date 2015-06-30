81ef9c878cf8882afe87921239c783b9f13c3f3d
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-06-30 18:06:28 -0400

    Swipe Left with no head logic

--- BlindWizardAppTests/GameSpec.m
+++ BlindWizardAppTests/GameSpec.m
@@ -19,8 +19,8 @@ describe(@"Game", ^{
         });
     });
     
-    context(@"when executing a swipe left", ^{
-        it(@"should shift the items on row left, set the head of the row to the tail, and notify the changes", ^{
+    context(@"when executing a swipe left with an object at the head", ^{
+        it(@"should shift the items on row left, set the head of the row to the tail, and notify changes for actual objects", ^{
             //context
             NSInteger row = 0;
             NSMutableArray *startData = [@[@1, @2, @0, @4] mutableCopy];
@@ -65,6 +65,44 @@ describe(@"Game", ^{
         });
     });
 
+    context(@"when executing a swipe left without an object at the head", ^{
+        it(@"should shift the items on row left, set the head of the row to the tail, and notify the changes for actual objects", ^{
+            //context
+            NSInteger row = 0;
+            NSMutableArray *startData = [@[@0, @2, @0, @4] mutableCopy];
+            NSMutableArray *endData = [@[@2, @0, @4, @0] mutableCopy];
+            sut.numRows = 1;
+            sut.numColumns = [startData count];
+            sut.data = startData;
+            id notificationMock = OCMObserverMock();
+            [[NSNotificationCenter defaultCenter] addMockObserver:notificationMock name:[Game ShiftLeftNotificationName] object:sut];
+            [[NSNotificationCenter defaultCenter] addMockObserver:notificationMock name:[Game MoveToRowTailNotificationName] object:sut];
+            [[notificationMock expect] notificationWithName:[Game ShiftLeftNotificationName]
+                                                     object:sut
+                                                   userInfo:[OCMArg checkWithBlock:^BOOL(NSDictionary *userInfo) {
+                expect([userInfo objectForKey:@"row"]).to.equal(@(row));
+                expect([userInfo objectForKey:@"column"]).to.equal(@1);
+                return YES;
+            }]];
+            [[notificationMock expect] notificationWithName:[Game ShiftLeftNotificationName]
+                                                     object:sut
+                                                   userInfo:[OCMArg checkWithBlock:^BOOL(NSDictionary *userInfo) {
+                expect([userInfo objectForKey:@"row"]).to.equal(@(row));
+                expect([userInfo objectForKey:@"column"]).to.equal(@3);
+                return YES;
+            }]];
+            
+            //because
+            [sut executeShiftLeftOnRow:row];
+            
+            //expect
+            expect(sut.data).to.equal(endData);
+            OCMVerifyAll(notificationMock);
+            
+            //cleanup
+            [[NSNotificationCenter defaultCenter] removeObserver:notificationMock];
+        });
+    });
 });
 
 SpecEnd
