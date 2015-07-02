71a38336bc6eb7019ecfc35daf47aca725e8680a
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-07-01 20:44:39 -0400

    GameActionFlow test structure

---
+++ BlindWizardApp/GameLogic/GameActionFlow.h
@@ -0,0 +1,13 @@
+//
+//  GameActionFlow.h
+//  BlindWizardApp
+//
+//  Created by N A on 7/1/15.
+//  Copyright (c) 2015 Adronitis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameActionFlow : NSObject
+
+@end
---
+++ BlindWizardApp/GameLogic/GameActionFlow.m
@@ -0,0 +1,13 @@
+//
+//  GameActionFlow.m
+//  BlindWizardApp
+//
+//  Created by N A on 7/1/15.
+//  Copyright (c) 2015 Adronitis. All rights reserved.
+//
+
+#import "GameActionFlow.h"
+
+@implementation GameActionFlow
+
+@end
