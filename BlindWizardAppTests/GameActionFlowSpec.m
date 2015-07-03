36e966a8533dac55564c85b16b312930ae435109
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-07-03 01:24:53 -0400

    Modified GameActionFlowSpec

--- BlindWizardAppTests/GameActionFlowSpec.m
+++ BlindWizardAppTests/GameActionFlowSpec.m
@@ -7,35 +7,53 @@
 SpecBegin(GameActionFlow)
 
 describe(@"GameActionFlow", ^{
+    __block GameActionFlow *sut;
+    
+    beforeEach(^{
+        sut = [[GameActionFlow alloc] init];
+    });
+    
     context(@"input commands", ^{
         context(@"when starting the game", ^{
-            it(@"should push the command on the queue and attempt to set the current game action", ^{
+            it(@"should push the command on the queue", ^{
                 
             });
         });
         
         context(@"when calling the next wave", ^{
-            it(@"should push the command on the queue and attempt to set the current game action", ^{
+            it(@"should push the command on the queue", ^{
                 
             });
         });
         
         context(@"when swiping left", ^{
-            it(@"should push the command on the queue and attempt to set the current game action", ^{
+            it(@"should push the command on the queue", ^{
                 
             });
         });
         
-        context(@"when swping right", ^{
-            it(@"should push the command on the queue and attempt to set the current game action", ^{
+        context(@"when swiping right", ^{
+            it(@"should push the command on the queue", ^{
                 
             });
         });
     });
     
     context(@"game actions", ^{
-        context(@"when there is a game action in the queue and the sut is ready", ^{
-            it(@"should pop a game action off the queue, set it as the current game action, and set the sut to not ready for the duration of the game action", ^{
+        context(@"when the queue gets a new item", ^{
+            it(@"should attempt to run a game action", ^{
+                
+            });
+        });
+        
+        context(@"when the sut becomes ready", ^{
+            it(@"should attempt to run a game action", ^{
+                
+            });
+        });
+        
+        context(@"when attempting to run a game action and the sut is ready and there is an item in the queue", ^{
+            it(@"should pop a it off the queue, set not ready for the duration of the game action, and notify", ^{
                 
             });
         });
