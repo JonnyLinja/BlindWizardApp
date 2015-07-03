a64eb4e8b76bae108ca6e21de8e82bf8e7f61aba
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-07-03 00:55:13 -0400

    GameActionFlow framework

--- BlindWizardAppTests/GameActionFlowSpec.m
+++ BlindWizardAppTests/GameActionFlowSpec.m
@@ -7,7 +7,39 @@
 SpecBegin(GameActionFlow)
 
 describe(@"GameActionFlow", ^{
-
+    context(@"input commands", ^{
+        context(@"when starting the game", ^{
+            it(@"should push the command on the queue and attempt to set the current game action", ^{
+                
+            });
+        });
+        
+        context(@"when calling the next wave", ^{
+            it(@"should push the command on the queue and attempt to set the current game action", ^{
+                
+            });
+        });
+        
+        context(@"when swiping left", ^{
+            it(@"should push the command on the queue and attempt to set the current game action", ^{
+                
+            });
+        });
+        
+        context(@"when swping right", ^{
+            it(@"should push the command on the queue and attempt to set the current game action", ^{
+                
+            });
+        });
+    });
+    
+    context(@"game actions", ^{
+        context(@"when there is a game action in the queue and the sut is ready", ^{
+            it(@"should pop a game action off the queue, set it as the current game action, and set the sut to not ready for the duration of the game action", ^{
+                
+            });
+        });
+    });
 });
 
 SpecEnd
