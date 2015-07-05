f1d1b5d3d591489ad597735a7813f43932508e44
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-07-05 04:39:03 -0400

    fixed logic again for calc num rows cols

--- BlindWizardApp/Grid/GridCalculator.m
+++ BlindWizardApp/Grid/GridCalculator.m
@@ -7,12 +7,14 @@
 //
 
 #import "GridCalculator.h"
+#import <tgmath.h>
 
 @interface GridCalculator ()
 @property (nonatomic, assign) NSInteger numRows;
 @property (nonatomic, assign) NSInteger numColumns;
 @property (nonatomic, assign) CGSize size;
 @property (nonatomic, assign) CGFloat verticalPadding;
+@property (nonatomic, assign) CGFloat horizontalPadding;
 @end
 
 @implementation GridCalculator
@@ -24,18 +26,36 @@
     
     CGFloat maxHeight = self.numRows * self.squareHeight;
     self.verticalPadding = size.height - maxHeight;
+    
+    CGFloat maxWidth = self.numColumns * self.squareWidth;
+    CGFloat maxHorizontalPadding = size.width - maxWidth;
+    self.horizontalPadding = maxHorizontalPadding / (self.numColumns-1);
 }
 
 - (NSInteger) calculateRowForYPos:(CGFloat)yPos {
     return self.numRows - ((yPos-self.verticalPadding) / self.squareHeight);
 }
 
-// roundf
+// round
 - (CGPoint) calculatePointForRow:(NSInteger)row column:(NSInteger)column {
+    //x
+    CGFloat x;
+    if(column == 0) {
+        //first
+        x = 0;
+    }else if(column == self.numColumns-1) {
+        //last
+        x = self.size.width - self.squareWidth;
+    }else {
+        //middle
+        x = (column * self.squareWidth);
+        x = -1;
+    }
+    
     //y
     CGFloat y = self.size.height - ((row+1) * self.squareHeight);
     
-    return CGPointMake(0, y);
+    return CGPointMake(x, y);
 }
 
 @end
--- BlindWizardAppTests/GridCalculatorSpec.m
+++ BlindWizardAppTests/GridCalculatorSpec.m
@@ -9,6 +9,7 @@
 @property (nonatomic, assign) NSInteger numColumns;
 @property (nonatomic, assign) CGSize size;
 @property (nonatomic, assign) CGFloat verticalPadding;
+@property (nonatomic, assign) CGFloat horizontalPadding;
 @end
 
 SpecBegin(GridCalculator)
@@ -23,7 +24,7 @@ describe(@"GridCalculator", ^{
     context(@"when calculating num rows and cols for size", ^{
         it(@"should save size, calculate & save verticalPadding, and set numRows & numColumns", ^{
             //context
-            CGSize size = CGSizeMake(303, 604);
+            CGSize size = CGSizeMake(327, 604);
             sut.squareWidth = 30;
             sut.squareHeight = 40;
             
@@ -35,38 +36,39 @@ describe(@"GridCalculator", ^{
             expect(sut.numColumns).to.equal(10);
             expect(sut.size).to.equal(size);
             expect(sut.verticalPadding).to.equal(4);
+            expect(sut.horizontalPadding).to.equal(3);
         });
     });
     
-    context(@"when calculating row for y pos", ^{
-        it(@"return a row based on square height and size height and verticalPadding", ^{
-            //context
+    context(@"after calculate rows and cols", ^{
+        beforeEach(^{
             sut.squareWidth = 30;
             sut.squareHeight = 40;
             [sut calculateNumberOfRowsAndColumnsForSize:CGSizeMake(303, 604)];
-            
-            //because
-            NSInteger row = [sut calculateRowForYPos:83];
-            
-            //expect
-            expect(row).to.equal(13);
         });
-    });
-    
-    //TODO: this is a lot harder as needs to be perfectly bottom aligned AND spread out horizontally to hit end screen
-    context(@"when calculating point for row and column", ^{
-        it(@"return a cgpoint origin of that grid position", ^{
-            //context
-            CGPoint finalPoint = CGPointMake(0, 444);
-            sut.squareWidth = 30;
-            sut.squareHeight = 40;
-            [sut calculateNumberOfRowsAndColumnsForSize:CGSizeMake(303, 604)];
-            
-            //because
-            CGPoint point = [sut calculatePointForRow:3 column:4];
-            
-            //expect
-            expect(point).to.equal(finalPoint);
+        
+        context(@"when calculating row for y pos", ^{
+            it(@"return a row based on square height and size height and verticalPadding", ^{
+                //because
+                NSInteger row = [sut calculateRowForYPos:83];
+                
+                //expect
+                expect(row).to.equal(13);
+            });
+        });
+        
+        //TODO: this is a lot harder as needs to be perfectly bottom aligned AND spread out horizontally to hit end screen
+        context(@"when calculating point for row and column", ^{
+            it(@"return a cgpoint origin of that grid position", ^{
+                //context
+                CGPoint finalPoint = CGPointMake(-1, 444);
+                
+                //because
+                CGPoint point = [sut calculatePointForRow:3 column:4];
+                
+                //expect
+                expect(point).to.equal(finalPoint);
+            });
         });
     });
 });
