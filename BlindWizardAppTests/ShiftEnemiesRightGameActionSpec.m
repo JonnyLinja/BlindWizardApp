ecac2f4750eb2a0f149d016babccea0966fece81
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-07-25 17:24:38 -0400

    ShiftEnemiesRight handles negative game board values

--- BlindWizardAppTests/ShiftEnemiesRightGameActionSpec.m
+++ BlindWizardAppTests/ShiftEnemiesRightGameActionSpec.m
@@ -28,9 +28,9 @@ describe(@"ShiftEnemiesRightGameAction", ^{
     context(@"when executing", ^{
         it(@"should shift the items on row right, set the tail of the row to the head, and notify changes for actual objects", ^{
             //context
-            NSMutableArray *startData = [@[@0, @3, @0, @0, @1, @0, @2, @4] mutableCopy];
-            NSMutableArray *endData = [@[@0, @0, @3, @0, @4, @1, @0, @2] mutableCopy];
-            OCMStub([gameBoardMock numRows]).andReturn(2);
+            NSMutableArray *startData = [@[@0, @3, @-1, @0, @1, @0, @2, @4, @-1, @3, @-1, @-2] mutableCopy];
+            NSMutableArray *endData = [@[@0, @0, @3, @0, @4, @1, @0, @2, @-1, @0, @3, @-2] mutableCopy];
+            OCMStub([gameBoardMock numRows]).andReturn(3);
             OCMStub([gameBoardMock numColumns]).andReturn(4);
             OCMStub([gameBoardMock data]).andReturn(startData);
             id notificationMock = OCMObserverMock();
@@ -57,6 +57,13 @@ describe(@"ShiftEnemiesRightGameAction", ^{
                 expect([userInfo objectForKey:@"column"]).to.equal(@0);
                 return YES;
             }]];
+            [[notificationMock expect] notificationWithName:GameUpdateShiftEnemyRight
+                                                     object:sut
+                                                   userInfo:[OCMArg checkWithBlock:^BOOL(NSDictionary *userInfo) {
+                expect([userInfo objectForKey:@"row"]).to.equal(@2);
+                expect([userInfo objectForKey:@"column"]).to.equal(@1);
+                return YES;
+            }]];
             [[notificationMock expect] notificationWithName:GameUpdateMoveEnemyToRowHead
                                                      object:sut
                                                    userInfo:[OCMArg checkWithBlock:^BOOL(NSDictionary *userInfo) {
@@ -69,6 +76,8 @@ describe(@"ShiftEnemiesRightGameAction", ^{
             sut.row = 0;
             [sut execute];
             sut.row = 1;
+            [sut execute];
+            sut.row = 2;
             [sut execute];
             
             //expect
@@ -83,7 +92,7 @@ describe(@"ShiftEnemiesRightGameAction", ^{
     context(@"when the row has at least one enemy", ^{
         it(@"should be valid", ^{
             //context
-            NSMutableArray *data = [@[@0, @0, @1, @1] mutableCopy];
+            NSMutableArray *data = [@[@-1, @0, @1, @1] mutableCopy];
             OCMStub([gameBoardMock numRows]).andReturn(2);
             OCMStub([gameBoardMock numColumns]).andReturn(2);
             OCMStub([gameBoardMock data]).andReturn(data);
@@ -100,7 +109,7 @@ describe(@"ShiftEnemiesRightGameAction", ^{
     context(@"when the row has no enemies", ^{
         it(@"should be invalid", ^{
             //context
-            NSMutableArray *data = [@[@0, @0, @1, @1] mutableCopy];
+            NSMutableArray *data = [@[@-1, @0, @1, @1] mutableCopy];
             OCMStub([gameBoardMock numRows]).andReturn(2);
             OCMStub([gameBoardMock numColumns]).andReturn(2);
             OCMStub([gameBoardMock data]).andReturn(data);
